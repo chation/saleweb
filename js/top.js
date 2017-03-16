6bb72f2fa8bc9b0fd18fd7fb430af97dd8d48e27
chation <chation@qq.com>
2017-03-16 15:42:14 +0800

    create

---
+++ js/top.js
@@ -0,0 +1,8 @@
+﻿//返回顶部插件
+(function ($) {
+    jQuery.fn.scrollTo = function (speed) {
+        var targetOffset = $(this).offset().top;
+        $('html.body').stop().animate({ scrollTop: targetOffset }, speed);
+        return this;
+    };
+})(jQuery);
